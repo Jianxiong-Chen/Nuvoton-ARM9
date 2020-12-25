c1018dd9f29b6a1511409dffa30a3eb64ff46b86
chenjianxiong <jianxiongchen@xcharge.com>
2020-12-25 20:34:29 +0800

    buildroot update 20201225

---
+++ NUC970/NUC972DF62Y/tools/970_tools/dropber/build_yaffs2.sh
@@ -0,0 +1,1 @@
+sudo mkyaffs2 --inband-tags -p 2048 rootfs rootfs_cjx_yaffs2.img
---
+++ NUC970/NUC972DF62Y/tools/970_tools/dropber/rootfs/etc/init.d/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i =0;
+	while(1)
+	{
+		sleep(1);
+		printf("this is app hello test count%d\n",i++);
+	}
+
+
+
+
+}
